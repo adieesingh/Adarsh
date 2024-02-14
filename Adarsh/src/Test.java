5b0ee8138c938b26e98b3667049dbdcf4d7d8008
Adarsh <Adarsh@DESKTOP-NK5NS6E>
2024-02-14 09:51:50 +0530

    Edit

--- Adarsh/src/Test.java
+++ Adarsh/src/Test.java
@@ -3,6 +3,15 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello worl");
 		System.out.println("its okay");
+		int i = 19;
+		if (i>18) {
+			System.out.println("You can vote");
+			
+		} else {
+			System.out.println("Cant vote");
+
+		}
+		System.out.println(i);
 	}
 
 }
