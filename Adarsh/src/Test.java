f702faaeb5b29b0a09b38f47d4f361cb4cb03de7
Adarsh <Adarsh@DESKTOP-NK5NS6E>
2024-02-14 09:42:44 +0530

    okay2

--- Adarsh/src/Test.java
+++ Adarsh/src/Test.java
@@ -1,7 +1,8 @@
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello worl");
+		System.out.println("its okay");
 	}
 
 }
